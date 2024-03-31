473cf3b5581cd93e6775785ae394ff43ac05c331
Jason Pien <jasonpien@jasonsmbp27107.devices.brown.edu>
2024-03-30 20:35:27 -0400

    save

---
+++ sms_sender_telegram.py
@@ -0,0 +1,18 @@
+import requests
+from config import TELEGRAM_BOT_API, TELEGRAM_CHATID
+def send_telegram_message(chat_id, message, bot_token):
+    send_url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    params = {
+        'chat_id': chat_id,
+        'text': message,
+    }
+    response = requests.post(send_url, data=params)
+    return response.json()
+
+# Test
+if __name__ == "__main__":
+    bot_token = TELEGRAM_BOT_API
+    chat_id = TELEGRAM_CHATID
+    message = 'Hello! This is your daily summary from Telegram Bot.'
+    result = send_telegram_message(chat_id, message, bot_token)
+    print(result)
