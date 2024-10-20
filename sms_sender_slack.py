b29e31e816534fbe594885b2c4d8156f36dde974
Jason Pien <jason_pien@brown.edu>
2024-10-20 18:41:19 -0400

    remove references to config since now use github secrets

--- sms_sender_slack.py
+++ sms_sender_slack.py
@@ -1,5 +1,8 @@
 import requests
-from config import SLACK_BOT_OAUTH
+
+# Access sensitive data from environment variables
+SLACK_BOT_OAUTH = os.getenv('SLACK_BOT_OAUTH')
+
 
 import os
 # Access sensitive data from environment variables
